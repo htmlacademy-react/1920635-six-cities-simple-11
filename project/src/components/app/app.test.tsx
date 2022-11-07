315e78441bb6907a4381cd2c753cbc76f67939e8
AndreyChvartatskii <chvartatskii@mail.ru>
2022-11-07 23:08:04 +0300

    Второе задание

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -2,7 +2,7 @@ import {render, screen} from '@testing-library/react';
 import App from './app';
 
 test('Renders app-component', () => {
-  render(<App />);
+  render(<App cardNumber={0} />);
   const textElement = screen.getByText(/Hello, world!/i);
   expect(textElement).toBeInTheDocument();
 });
--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppScreenProps = {
+  cardNumber: number;
+}
+
+function App({cardNumber}: AppScreenProps): JSX.Element {
+  return (
+    <MainPage cardsNumber={cardNumber}/>
+  );
 }
 
 export default App;
